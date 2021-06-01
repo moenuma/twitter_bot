9bd277e9d9c092255ab9baedfd903dba426dbf7e
moenuma <moenumasawa@gmail.com>
2021-06-01 23:13:06 +0900

    Add follow_back function

--- fan_account.py
+++ fan_account.py
@@ -20,7 +20,7 @@ def who_to_follow(twitterID, name):
             if name in tweet.user.description.lower():
                 if tweet.user.screen_name not in follow_list:
                     follow_list.append(tweet.user.screen_name)
-
+    print(follow_list)
     return follow_list
 
 def retweet_popular_tweets():
@@ -29,8 +29,20 @@ def retweet_popular_tweets():
 def like_popular_tweets():
     pass
 
-def followback():
-    pass
+def follow_back():
+    followers = api.followers()
+    following = api.friends()
+    for follower in followers:
+        if follower not in following:
+            api.create_friendship(screen_name=follower.screen_name)
+
+def is_fanaccount(twitterID, fan):
+    '''
+    Determines whether the selected user is also a fan account
+    '''
+    # 1. if the figure's name is included in a bio
+    # 2. if the account follows the figure
+
 
 def main():
     valid = False
@@ -39,7 +51,7 @@ def main():
         twitterID = input("Enter Twitter ID of the figure that you are fan of: @")
         
         try:
-            user_info = api.get_user(screen_name=twitterID)
+            twitter_info = api.get_user(screen_name=twitterID)
             valid = True
         except tweepy.error.TweepError:
             print("User not found")
@@ -48,6 +60,10 @@ def main():
     if valid:
 
         api.create_friendship(screen_name=twitterID)
+        #lst = [follower.screen_name for follower in api.followers_ids()]
+        #print(lst)
+        #follow_back()
+        #print(is_fanaccount(twitterID, "sorryiamonadiet"))
         follow_list = who_to_follow(twitterID, name)
 
     
