45c447836314b9bb76f7fa92cbd6feb9819a3843
moenuma <moenumasawa@gmail.com>
2021-06-02 11:04:49 +0900

    Add hashtag_search function

--- fan_account.py
+++ fan_account.py
@@ -2,6 +2,8 @@ from credentials import *
 import tweepy
 import time
 import datetime
+import re
+from collections import Counter
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
@@ -28,6 +30,27 @@ def retweet_popular_tweets():
 
 def like_popular_tweets():
     pass
+
+def hashtag_search(twitterID):
+    '''
+    Search for the current popular hashtags that your fellow fans are using
+    '''
+    hashtag_dict = {}
+    results = tweepy.Cursor(api.search, q=twitterID).items(200)
+    for tweet in results:
+        hashtags = re.findall(r"#(\w+)", tweet.text)
+        for hashtag in hashtags:
+            if not hashtag.isdigit():
+                if hashtag in hashtag_dict.keys():
+                    hashtag_dict[hashtag] += 1
+                else:
+                    hashtag_dict[hashtag] = 1
+
+    top10_hashtags = Counter(hashtag_dict).most_common(10)
+    print("\nTop 10 Popular Hashtags at that moment: ")
+    for hashtag in top10_hashtags:
+        print('\t#{}'.format(hashtag[0]))
+    
 
 def follow_back():
     followers = api.followers()
@@ -64,7 +87,8 @@ def main():
         #print(lst)
         #follow_back()
         #print(is_fanaccount(twitterID, "sorryiamonadiet"))
-        follow_list = who_to_follow(twitterID, name)
+        #follow_list = who_to_follow(twitterID, name)
+        hashtag_search(twitterID)
 
     
 
